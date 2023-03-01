213f998544b3fea01bc5052d9a1e06bda43846b6
sshekhar17 <shshekha@eng.ucsd.edu>
2023-02-28 21:09:50 -0500

    fixed shell scripts

--- run_null_experiments.sh
+++ run_null_experiments.sh
@@ -8,60 +8,59 @@ function update_seed(){
 }
 ####### Null Experiments 
 SEED=$(update_seed $SEED)
-#python3 ExperimentNull.py --pdf_plot --save_fig -n 200 -r 500 -d 10 --seed $SEED
+python3 ExperimentNull.py --pdf_plot --save_fig -n 200 -r 500 -d 10 --seed $SEED
 SEED=$(update_seed $SEED)
-#python3 ExperimentNull.py --pdf_plot  --save_fig -n 200 -r 500 -d 100 --seed $SEED
+python3 ExperimentNull.py --pdf_plot  --save_fig -n 200 -r 500 -d 100 --seed $SEED
 
 SEED=$(update_seed $SEED)
-#python3 ExperimentNull.py --pdf_plot --save_fig -n 200 -r 500 -d 10 -k RationalQuadratic --seed $SEED
+python3 ExperimentNull.py --pdf_plot --save_fig -n 200 -r 500 -d 10 -k RationalQuadratic --seed $SEED
 SEED=$(update_seed $SEED)
-#python3 ExperimentNull.py --pdf_plot  --save_fig -n 200 -r 500 -d 100 -k RationalQuadratic --seed $SEED
+python3 ExperimentNull.py --pdf_plot  --save_fig -n 200 -r 500 -d 100 -k RationalQuadratic --seed $SEED
 
 SEED=$(update_seed $SEED)
-#python3 ExperimentNull.py --pdf_plot --save_fig -n 200 -r 500 -d 10 -exp dCov --seed $SEED
+python3 ExperimentNull.py --pdf_plot --save_fig -n 200 -r 500 -d 10 -exp dCov --seed $SEED
 SEED=$(update_seed $SEED)
-#python3 ExperimentNull.py --pdf_plot  --save_fig -n 200 -r 500 -d 100 -exp dCov --seed $SEED
+python3 ExperimentNull.py --pdf_plot  --save_fig -n 200 -r 500 -d 100 -exp dCov --seed $SEED
 
 
 SEED=$(update_seed $SEED)
-#python3 ExperimentNull.py --typeI_plot --save_fig -n 200 -r 500 -d 10 -exp HSIC --seed $SEED
+python3 ExperimentNull.py --typeI_plot --save_fig -n 200 -r 500 -d 10 -exp HSIC --seed $SEED
 SEED=$(update_seed $SEED)
-#python3 ExperimentNull.py --typeI_plot --save_fig -n 200 -r 500 -d 100 -exp HSIC --seed $SEED
+python3 ExperimentNull.py --typeI_plot --save_fig -n 200 -r 500 -d 100 -exp HSIC --seed $SEED
 
 SEED=$(update_seed $SEED)
-#python3 ExperimentNull.py --typeI_plot --save_fig -n 200 -r 500 -d 10 -k RationalQuadratic --seed $SEED
+python3 ExperimentNull.py --typeI_plot --save_fig -n 200 -r 500 -d 10 -k RationalQuadratic --seed $SEED
 SEED=$(update_seed $SEED)
-#python3 ExperimentNull.py --typeI_plot  --save_fig -n 200 -r 500 -d 100 -k RationalQuadratic --seed $SEED
+python3 ExperimentNull.py --typeI_plot  --save_fig -n 200 -r 500 -d 100 -k RationalQuadratic --seed $SEED
 
 SEED=$(update_seed $SEED)
-#python3 ExperimentNull.py --typeI_plot --save_fig -n 200 -r 500 -d 10 -exp dCov --seed $SEED
+python3 ExperimentNull.py --typeI_plot --save_fig -n 200 -r 500 -d 10 -exp dCov --seed $SEED
 SEED=$(update_seed $SEED)
-#python3 ExperimentNull.py --typeI_plot  --save_fig -n 200 -r 500 -d 100 -exp dCov --seed $SEED
+python3 ExperimentNull.py --typeI_plot  --save_fig -n 200 -r 500 -d 100 -exp dCov --seed $SEED
 
 SEED=$(update_seed $SEED)
-#python3 ExperimentNull.py --normality_test  --save_fig -n 200 -r 200 -nt 250 -d 10 -exp HSIC --seed $SEED
+python3 ExperimentNull.py --normality_test  --save_fig -n 200 -r 200 -nt 250 -d 10 -exp HSIC --seed $SEED
 SEED=$(update_seed $SEED)
-#python3 ExperimentNull.py --normality_test  --save_fig -n 200 -r 200 -nt 250 -d 10 -exp dCov --seed $SEED
+python3 ExperimentNull.py --normality_test  --save_fig -n 200 -r 200 -nt 250 -d 10 -exp dCov --seed $SEED
 
 SEED=$(update_seed $SEED)
-#python3 ExperimentNull.py --normality_test  --save_fig -n 200 -r 200 -nt 250 -d 100 -exp HSIC --seed $SEED
+python3 ExperimentNull.py --normality_test  --save_fig -n 200 -r 200 -nt 250 -d 100 -exp HSIC --seed $SEED
 SEED=$(update_seed $SEED)
 python3 ExperimentNull.py --normality_test  --save_fig -n 200 -r 200 -nt 250 -d 100 -exp dCov --seed $SEED
 
 
 SEED=$(update_seed $SEED)
-#python3 ExperimentNull.py --second_moment -df 1  --save_fig -n 500 -r 500 -d 10 -k Linear --seed $SEED 
+python3 ExperimentNull.py --second_moment -df 1  --save_fig -n 500 -r 500 -d 10 -k Linear --seed $SEED 
 SEED=$(update_seed $SEED)
-#python3 ExperimentNull.py --second_moment -df 2  --save_fig -n 500 -r 500 -d 10 -k Linear --seed $SEED 
+python3 ExperimentNull.py --second_moment -df 2  --save_fig -n 500 -r 500 -d 10 -k Linear --seed $SEED 
 SEED=$(update_seed $SEED)
-#python3 ExperimentNull.py --second_moment -df 3  --save_fig -n 500 -r 500 -d 10 -k Linear --seed $SEED 
-SEED=$(update_seed $SEED)
+python3 ExperimentNull.py --second_moment -df 3  --save_fig -n 500 -r 500 -d 10 -k Linear --seed $SEED 
 
-#python3 ExperimentNull.py --second_moment -df 1  --save_fig -n 500 -r 500 -d 10 -exp dCov --seed $SEED 
 SEED=$(update_seed $SEED)
-#python3 ExperimentNull.py --second_moment -df 2  --save_fig -n 500 -r 500 -d 10 -exp dCov --seed $SEED 
+python3 ExperimentNull.py --second_moment -df 1  --save_fig -n 500 -r 500 -d 10 -exp dCov --seed $SEED 
 SEED=$(update_seed $SEED)
-#python3 ExperimentNull.py --second_moment -df 3  --save_fig -n 500 -r 500 -d 10 -exp dCov --seed $SEED 
+python3 ExperimentNull.py --second_moment -df 2  --save_fig -n 500 -r 500 -d 10 -exp dCov --seed $SEED 
 SEED=$(update_seed $SEED)
+python3 ExperimentNull.py --second_moment -df 3  --save_fig -n 500 -r 500 -d 10 -exp dCov --seed $SEED 
 
 
